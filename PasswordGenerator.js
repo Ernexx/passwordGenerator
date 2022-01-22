75cbeee0811946e236afd8a9973e3d582f9101c9
Ernexx <97474471+Ernexx@users.noreply.github.com>
2022-01-22 12:48:07 +0300

    Update PasswordGenerator.js

--- PasswordGenerator.js
+++ PasswordGenerator.js
@@ -1,8 +1,9 @@
 const letters = [
     'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 
     'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 
-    'n', 'N', 'o', 'O', 'p', 'P',  'q', 'Q', 'r', 'R', 's', 'S', 't', 
-    'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z'
+    'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 
+    'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z',
+    '!', '#', '-', '_', '@'
 ]
 
 function randomNumber(min, max){
@@ -16,7 +17,7 @@ function randomBool(){
 }
 
 function randomizeElement(){
-    if (randomBool() == true) return(letters[randomNumber(0, letters.length)]);
+    if (randomBool() == true) return(letters[randomNumber(0, letters.length - 1)]);
     return(randomNumber(0, 9));
 }
 
