e1e22f6e46ecf75a01d8b893ae7c0f45534be7af
Ernexx <BlayGYT@mail.ru>
2024-12-02 06:11:45 +0300

    remaster

--- PasswordGenerator.js
+++ PasswordGenerator.js
@@ -1,31 +1,15 @@
-const letters = [
+const symbols = [
     'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 
     'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 
     'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 
     'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z',
-    '!', '#', '-', '_', '@'
-]
-
-function randomNumber(min, max){
-    return(Math.round(Math.random() * (max - min) + min));
-}
-
-function randomBool(){
-    let randomValue = Math.round(Math.random());
-    if (randomValue == 1) return(true);
-    return(false);
-}
-
-function randomizeElement(){
-    if (randomBool() == true) return(letters[randomNumber(0, letters.length - 1)]);
-    return(randomNumber(0, 9));
-}
+    '!', '#', '-', '_', '@', '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9' ]
 
 function generatePassword(length){
-    let passwordElements = new Array(length);
     let password = '';
-    for (let i = 0; i < passwordElements.length; i++){
-        password += randomizeElement();
-    }
+    for (let i = 0; i < length; i++)
+        password += symbols[Math.round(Math.random() * symbols.length - 1)];
     return(password)
 }
+console.log(generatePassword(23))
